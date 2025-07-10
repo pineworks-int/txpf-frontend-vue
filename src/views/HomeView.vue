9654dde8e4dd9c27495dd636475d2fdd4e7fe6b1
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-07-10 23:38:38 +0200

    feat: Import HeroSection component into HomeView.vue and update template structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-
+import HeroSection from '../components/layout/HeroSection.vue'
 </script>
 
 <template>
   <div>
     <h1>Home Page</h1>
   </div>
+  <HeroSection />
+  <!-- project section will go here later -->
 </template>
