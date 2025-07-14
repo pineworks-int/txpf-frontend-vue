d4646c40d40afab71f4ab1b85bbebb43ed536818
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-07-14 22:52:11 +0200

    refactor: Simplify useProjects composable by integrating useProjectsStore for state management and enhancing error handling

--- src/composables/useProjects.ts
+++ src/composables/useProjects.ts
@@ -1,59 +1,50 @@
-import type { ProjectProps } from '@/types/project.type'
-import { ref } from 'vue'
-import supabase from '@/lib/supabase'
+import supabase from '@/lib/supabase';
+import { useProjectsStore } from '@/stores/projects';
 
 export function useProjects() {
-  const projects = ref<ProjectProps[]>([])
-  const isLoading = ref(false)
-  const error = ref<Error | null>(null)
+  const projectsStore = useProjectsStore();
 
   const fetchProjects = async () => {
     try {
-      isLoading.value = true
-      error.value = null
+      projectsStore.setLoading(true);
+      projectsStore.setError(null);
 
       const { data: fetchedProjects, error: fetchError } = await supabase
         .from('perso_projects')
-        .select('title, description, technologies, image_url, demo_url, github_repo_url, github_project_url')
+        .select('title, description, technologies, image_url, demo_url, github_repo_url, github_project_url');
 
       if (fetchError) {
-        throw new Error(fetchError.message)
+        throw new Error(fetchError.message);
       }
-
+      
       if (!fetchedProjects) {
-        projects.value = []
-        return
+        projectsStore.setProjects([]);
+        return;
       }
 
-      const transformedProjects = fetchedProjects.map((project) => {
-        return {
-          title: project.title,
-          description: project.description,
-          technologies: project.technologies,
-          imageUrl: project.image_url,
-          demoUrl: project.demo_url,
-          githubRepoUrl: project.github_repo_url,
-          githubProjectUrl: project.github_project_url,
-        }
-      })
+      const transformedProjects = fetchedProjects.map((project) => ({
+        title: project.title,
+        description: project.description,
+        technologies: project.technologies,
+        imageUrl: project.image_url,
+        demoUrl: project.demo_url,
+        githubRepoUrl: project.github_repo_url,
+        githubProjectUrl: project.github_project_url,
+      }));
 
-      projects.value = transformedProjects
-    }
-    catch (err) {
+      projectsStore.setProjects(transformedProjects);
+
+    } catch (err) {
       if (err instanceof Error) {
-        error.value = err
+        projectsStore.setError(err);
       }
-      console.error('Error fetching projects:', err)
+      console.error('Error fetching projects:', err);
+    } finally {
+      projectsStore.setLoading(false);
     }
-    finally {
-      isLoading.value = false
-    }
-  }
+  };
 
   return {
-    projects,
-    isLoading,
-    error,
     fetchProjects,
-  }
+  };
 }
