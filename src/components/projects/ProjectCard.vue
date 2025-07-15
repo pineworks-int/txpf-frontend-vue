91278fa4846811e497a613e3dfc41e72d9a779d2
Teh - "t3hx <77836986+t3hx@users.noreply.github.com>
2025-07-15 02:43:42 +0200

    feat: Add click handling to ProjectCard component for opening project detail modal, enhancing user interaction

--- src/components/projects/ProjectCard.vue
+++ src/components/projects/ProjectCard.vue
@@ -1,23 +1,35 @@
 <script setup lang="ts">
 import type { ProjectProps } from '@/types/project.type'
+import { useUiStore } from '@/stores/ui'
 
 const props = defineProps<ProjectProps>()
+
+const uiStore = useUiStore()
+const { openProjectDetailModal } = uiStore
+
+function handleCardClick() {
+  openProjectDetailModal(props)
+}
 </script>
 
 <template>
-  <div class="border rounded-lg p-4">
+  <div
+    class="border rounded-lg p-4 cursor-pointer hover:shadow-lg transition-shadow"
+    @click="handleCardClick" 
+  >
     <h3 class="text-lg font-bold">
       {{ props.title }}
     </h3>
-    <img :src="props.imageUrl" :alt="props.title" class="">
+    <img :src="props.imageUrl" :alt="props.title" class="my-2">
     <p>{{ props.description }}</p>
-    <p> Demo accessible at: <a :href="props.demoUrl" target="_blank">Click here to launch</a></p>
-    <p> Github repo @ <a :href="props.githubRepoUrl" target="_blank">{{ props.githubRepoUrl }}</a></p>
-    <p> Github project @ <a :href="props.githubProjectUrl" target="_blank">{{ props.githubProjectUrl }}</a></p>
     <div>
       Tech stack:
       <ul>
-        <li v-for="tech in props.technologies" :key="tech">
+        <li
+          v-for="tech in props.technologies" 
+          :key="tech"
+          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+        >
           {{ tech }}
           <!-- TODO: Add tech logos later -->
         </li>
