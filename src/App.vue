1c576e01284c28090fcfed3671592e63877038ca
t3hx <t3hx@nspace.link>
2025-07-21 00:38:10 +0200

    feat: Import ToastContainer component in App.vue for enhanced notification management

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
 import { onMounted } from 'vue'
 import { RouterView } from 'vue-router'
-import AuthModal from './components/auth/AuthModal.vue'
-import FooterSection from './components/layout/FooterSection.vue'
-import NavBar from './components/layout/NavBar.vue'
-import useAuth from './composables/useAuth'
+import AuthModal from '@/components/auth/AuthModal.vue'
+import FooterSection from '@/components/layout/FooterSection.vue'
+import NavBar from '@/components/layout/NavBar.vue'
+import ToastContainer from '@/components/ui/ToastContainer.vue'
+import useAuth from '@/composables/useAuth'
 
 const { listenForAuthState } = useAuth()
 
@@ -15,6 +16,7 @@ onMounted(() => {
 
 <template>
   <NavBar />
+  <ToastContainer />
   <main>
     <RouterView />
   </main>
