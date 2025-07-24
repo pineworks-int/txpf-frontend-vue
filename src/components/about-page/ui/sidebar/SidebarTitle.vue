500b51dcdc51ec3b335acd72bdfc9ad70bd0efba
t3hx <a230385707@gmail.com>
2025-07-24 17:59:13 +0200

    feat: Add SidebarTitle component to display the title in the About page sidebar

---
+++ src/components/about-page/ui/sidebar/SidebarTitle.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+  title: string
+}>()
+</script>
+
+<template>
+  <h3 class="text-lg font-semibold text-gray-800 mb-2 border-b border-gray-200 pb-2">
+    {{ title }}
+  </h3>
+</template>
