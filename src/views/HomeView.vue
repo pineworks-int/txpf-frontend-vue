0f7aa0c9c014cc0f5280de9f7002efd20d1f14a5
t3hx <a230385707@gmail.com>
2025-07-24 17:59:40 +0200

    feat: Refactor AboutMeView to utilize AboutLayout component for improved structure and remove unnecessary elements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import HeroSection from '@/components/layout/HeroSection.vue'
-import ProjectShowcase from '@/components/projects/ProjectShowcase.vue'
 import ProjectDetailModal from '@/components/projects/ProjectDetailModal.vue'
+import ProjectShowcase from '@/components/projects/ProjectShowcase.vue'
 </script>
 
 <template>
