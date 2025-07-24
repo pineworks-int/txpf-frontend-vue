30c19c7187c04fac6a6c2e2d28937129902fd4a9
t3hx <a230385707@gmail.com>
2025-07-24 17:58:46 +0200

    feat: Add SidebarItem component for displaying technology icons and text in the About page sidebar

---
+++ src/components/about-page/ui/sidebar/SidebarItem.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { getTechLogo } from '@/lib/tech-logos'
+
+const props = defineProps<{
+  iconKey: string
+  text: string
+}>()
+
+const icon = computed(() => getTechLogo(props.iconKey))
+</script>
+
+<template>
+  <div class="flex items-center space-x-3">
+    <span class="text-lg text-gray-500" v-html="icon" />
+    <span class="text-md text-gray-700">{{ text }}</span>
+  </div>
+</template>
