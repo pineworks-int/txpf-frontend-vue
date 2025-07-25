1dec6855d0edf478df7188ab7990d9889e0639fc
t3hx <a230385707@gmail.com>
2025-07-25 22:14:09 +0200

    feat: Add SidebarIconRowItem component for displaying icons with tooltips, enhancing sidebar functionality

---
+++ src/components/about-page/ui/sidebar/SidebarIconRowItem.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed } from 'vue'
+import { getIcon } from '@/lib/icons'
+
+const props = defineProps<{
+  iconKey: string
+  text: string
+}>()
+
+const icon = computed(() => getIcon(props.iconKey))
+</script>
+
+<template>
+  <div class="relative flex justify-center items-center p-2 border-1 border-neutral-400 rounded-md group">
+    <span
+      class="text-xl filter grayscale group-hover:grayscale-0 transition duration-300 ease-in-out"
+      v-html="icon"
+    />
+    <div
+      class="
+        absolute
+        bottom-full
+        mb-2
+        px-2
+        py-1
+        text-xs
+        text-white
+        bg-gray-800
+        rounded-md
+        opacity-0
+        group-hover:opacity-100
+        transition-opacity
+        pointer-events-none
+        whitespace-nowrap
+      "
+    >
+      {{ text }}
+    </div>
+  </div>
+</template>
