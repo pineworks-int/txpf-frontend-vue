50b3a0fee27b9ac2097881e6d164a925e314e8e1
t3hx <a230385707@gmail.com>
2025-07-25 22:13:32 +0200

    feat: Add LanguageSwitch component for toggling between English and French, enhancing multilingual support in the About page

---
+++ src/components/about-page/ui/header/LanguageSwitch.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useContentStore } from '@/stores/content'
+
+const contentStore = useContentStore()
+
+function toggleLanguage() {
+  const newLang = contentStore.currentLanguage === 'en' ? 'fr' : 'en'
+  contentStore.setLanguage(newLang)
+}
+</script>
+
+<template>
+  <button
+    class="p-2 rounded-md hover:bg-gray-200 transition-colors"
+    @click="toggleLanguage"
+  >
+    <span v-if="contentStore.currentLanguage === 'en'" class="font-semibold">EN</span>
+    <span v-else class="font-semibold">FR</span>
+  </button>
+</template>
+
+<style scoped>
+
+</style>
