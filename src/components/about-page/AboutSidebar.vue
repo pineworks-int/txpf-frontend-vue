8d9c05296c0695b449f4a2a982babc88e30e6bba
t3hx <a230385707@gmail.com>
2025-08-03 02:16:37 +0200

    feat: Add AboutPopover component for dynamic content display and enhance AboutFooter and AboutHeader with popover functionality

--- src/components/about-page/AboutSidebar.vue
+++ src/components/about-page/AboutSidebar.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { computed } from 'vue'
+import { computed, onMounted, onUnmounted, ref } from 'vue'
 import SidebarIconRowItem from '@/components/about-page/ui/sidebar/SidebarIconRowItem.vue'
 import SidebarItem from '@/components/about-page/ui/sidebar/SidebarItem.vue'
 import SidebarLinkItem from '@/components/about-page/ui/sidebar/SidebarLinkItem.vue'
 import SidebarSection from '@/components/about-page/ui/sidebar/SidebarSection.vue'
+import { getIcon } from '@/lib/icons'
 import { useContentStore } from '@/stores/content'
 
 const contentStore = useContentStore()
@@ -21,8 +22,9 @@ const sidebarContent = computed(() => {
   }
 })
 
+// ~-- Data Structures ---
 const contactInfo = [
-  { iconKey: 'mail', text: 'link', href: 'mailto:' },
+  { iconKey: 'mail', text: 'xxxx@xxxxxx.xxxx', href: 'mailto:xxxx@xxxxxx.xxxx' },
   { iconKey: 'phone', text: '+XX X XX XX XX XX', textToCopy: '+XXXXXXXXXXXX' },
   { iconKey: 'linkedin', text: 'linkedin', href: 'https://www.linkedin.com' },
   { iconKey: 'github', text: 'github', href: 'https://github.com' },
@@ -98,62 +100,160 @@ const hobbies = computed(() => [
   { iconKey: 'hardware', text: sidebarContent.value.hobbiesContent.hardware },
   { iconKey: 'coding', text: sidebarContent.value.hobbiesContent.code },
 ])
+
+// ~-- States ---
+const sidebarContainer = ref<HTMLElement | null>(null)
+const isScrollable = ref(false)
+const isScrolledToBottom = ref(false)
+
+const downArrowIcon = computed(() => getIcon('a_arr_d'))
+const upArrowIcon = computed(() => getIcon('a_arr_u'))
+
+const currentIcon = computed(() => {
+  return isScrolledToBottom.value ? upArrowIcon.value : downArrowIcon.value
+})
+
+const currentAriaLabel = computed(() => {
+  return isScrolledToBottom.value ? 'Scroll to top' : 'Scroll to bottom'
+})
+
+// ~-- Functions ---
+function scrollToBottom() {
+  if (sidebarContainer.value) {
+    sidebarContainer.value.scrollTo({
+      top: sidebarContainer.value.scrollHeight,
+      behavior: 'smooth',
+    })
+  }
+}
+
+function scrollToTop() {
+  if (sidebarContainer.value) {
+    sidebarContainer.value.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+  }
+}
+
+function handleScrollBtnClick() {
+  if (isScrolledToBottom.value) {
+    scrollToTop()
+  }
+  else {
+    scrollToBottom()
+  }
+}
+
+function handleScroll() {
+  const elem = sidebarContainer.value
+  if (elem) {
+    const atBottom = elem.scrollHeight - elem.scrollTop <= elem.clientHeight + 1
+    isScrolledToBottom.value = atBottom
+  }
+}
+
+function checkScrollability() {
+  if (sidebarContainer.value) {
+    isScrollable.value = sidebarContainer.value.scrollHeight > sidebarContainer.value.clientHeight
+    handleScroll()
+  }
+}
+
+// ~-- Lifecycle Hooks ---
+onMounted(() => {
+  if (sidebarContainer.value) {
+    checkScrollability()
+    isScrollable.value = sidebarContainer.value.scrollHeight > sidebarContainer.value.clientHeight
+    sidebarContainer.value.addEventListener('scroll', handleScroll)
+  }
+})
+
+onUnmounted(() => {
+  if (sidebarContainer.value) {
+    sidebarContainer.value.removeEventListener('scroll', handleScroll)
+  }
+})
 </script>
 
 <template>
-  <div v-if="sidebarContent.sections" class="h-full w-full p-4 overflow-y-auto">
-    <SidebarSection :title="sidebarContent.sections.contact">
-      <div class="space-y-3 ml-2">
-        <div class="space-y-2">
-          <SidebarLinkItem
-            v-for="item in contactInfo"
-            :key="item.text"
-            :icon-key="item.iconKey"
-            :text="item.text"
-            :href="item.href"
-            :text-to-copy="item.textToCopy"
-          />
-        </div>
-      </div>
-    </SidebarSection>
-    <SidebarSection :title="sidebarContent.sections.hardskills">
-      <div class="space-y-3 ml-2">
-        <div v-for="subsection in hardSkills" :key="subsection.title">
-          <h4 class="text-xs font-semibold text-gray-600 mb-2">
-            {{ subsection.title }}
-          </h4>
-
-          <div
-            v-if="subsection.title !== sidebarContent.hardskillsSubs.os"
-            class="space-y-1"
-          >
-            <SidebarItem
-              v-for="item in subsection.items"
+  <div
+    v-if="sidebarContent.sections"
+    class="h-full w-full"
+  >
+    <div
+      ref="sidebarContainer"
+      class="relative h-full w-full p-4 overflow-y-auto hide-scrollbar"
+    >
+      <SidebarSection :title="sidebarContent.sections.contact">
+        <div class="space-y-3 ml-2">
+          <div class="space-y-2">
+            <SidebarLinkItem
+              v-for="item in contactInfo"
               :key="item.text"
               :icon-key="item.iconKey"
               :text="item.text"
-            />
-          </div>
-          <div
-            v-else
-            class="flex flex-row space-x-4 ml-2"
-          >
-            <SidebarIconRowItem
-              v-for="item in subsection.items"
-              :key="item.text"
-              :icon-key="item.iconKey"
-              :text="item.text"
+              :href="item.href"
+              :text-to-copy="item.textToCopy"
             />
           </div>
         </div>
-      </div>
-    </SidebarSection>
-    <SidebarSection :title="sidebarContent.sections.softskills" :items="softSkills" />
-    <SidebarSection :title="sidebarContent.sections.languages" :items="languages" />
-    <SidebarSection :title="sidebarContent.sections.hobbies" :items="hobbies" />
+      </SidebarSection>
+      <SidebarSection :title="sidebarContent.sections.hardskills">
+        <div class="space-y-3 ml-2">
+          <div v-for="subsection in hardSkills" :key="subsection.title">
+            <h4 class="text-xs font-semibold text-gray-600 mb-2">
+              {{ subsection.title }}
+            </h4>
+            <div
+              v-if="subsection.title !== sidebarContent.hardskillsSubs.os"
+              class="space-y-1"
+            >
+              <SidebarItem
+                v-for="item in subsection.items"
+                :key="item.text"
+                :icon-key="item.iconKey"
+                :text="item.text"
+              />
+            </div>
+            <div
+              v-else
+              class="flex flex-row space-x-4 ml-2"
+            >
+              <SidebarIconRowItem
+                v-for="item in subsection.items"
+                :key="item.text"
+                :icon-key="item.iconKey"
+                :text="item.text"
+              />
+            </div>
+          </div>
+        </div>
+      </SidebarSection>
+      <SidebarSection :title="sidebarContent.sections.softskills" :items="softSkills" />
+      <SidebarSection :title="sidebarContent.sections.languages" :items="languages" />
+      <SidebarSection :title="sidebarContent.sections.hobbies" :items="hobbies" />
+    </div>
+    <button
+      v-if="isScrollable"
+      class="absolute bottom-4 right-4 z-10 flex h-10 w-10 items-center justify-center rounded-full bg-slate-200/50 text-slate-700 shadow-lg backdrop-blur-sm transition-colors hover:bg-slate-300/70"
+      :aria-label="currentAriaLabel"
+      @click="handleScrollBtnClick"
+    >
+      <span v-html="currentIcon" />
+    </button>
   </div>
 </template>
 
 <style lang="css" scoped>
-
+.hide-scrollbar {
+  /* IE and Edge */
+  -ms-overflow-style: none;
+  /* Firefox */
+  scrollbar-width: none;
+}
+/* webkit fix */
+:deep(.hide-scrollbar::-webkit-scrollbar) {
+  display: none;
+}
 </style>
